f470c74c506addaa17887a46d0fc795210bb8a1d
Gabriel Viana <gvianaraujo@gmail.com>
2023-05-03 21:48:45 -0300

    projeto criado

---
+++ src/main/java/br/com/gva/wisedelivery/WisedeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.gva.wisedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisedeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gva/wisedelivery/WisedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gva.wisedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
